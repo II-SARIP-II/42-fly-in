1dd2e343807eec6541d01b82f5f203db75cef384
pgougne <paris.gougne.05@gmail.com>
2026-05-05 15:44:58 +0200

    better movement count

--- src/output/output_constructor.py
+++ src/output/output_constructor.py
@@ -1,4 +1,4 @@
-from ..parsing import Drone
+from ..parsing.fly_in_class import Drone, ZoneType
 
 from typing import List, Dict
 
@@ -10,7 +10,9 @@ def build_output(drones: List[Drone]) -> Dict[int, int]:
         count[i] = count[i - 1] if i > 0 else 0
         for drone in drones:
             if (len(drone.path) > i and not drone.path[i].is_start
-                    and not drone.path[i - 1].is_end):
+                    and not drone.path[i - 1].is_end and drone.path[i - 1] != drone.path[i]):
+                if drone.path[i].zone == ZoneType.RESTRICTED:
+                    count[i] += 1
                 count[i] += 1
                 print(f"D{drone.drone_id}-{drone.path[i].name}", end=" ")
         print("\n", end="")
