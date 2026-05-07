84897aa1f7a24d2c97ca2bed90603dcbcb084385
pgougne <paris.gougne.05@gmail.com>
2026-05-07 16:37:57 +0200

    docstring for get dron pathes and amera

--- src/algorithm/get_drones_pathes.py
+++ src/algorithm/get_drones_pathes.py
@@ -5,7 +5,18 @@ import math
 
 
 class PathsFinding:
+    '''
+    Class to find the best solution of a graph for each drones using
+    a reverse djikstra and reservation table
+    '''
     def __init__(self, input_data: Input_Data) -> None:
+        '''
+        Initialisation of the class that find the best solution of a graph
+        for each drones using a reverse djikstra and reservation table
+        Param:
+            input_data: Input_Data = a class that store a list of drones,
+                list of hubs and list of connections
+        '''
         self.reservation_hub: Dict[str, Dict[int, int]] = {
             hub.name: {} for hub in input_data.hubs}
         self.res_conn: Dict[int, Dict[int, int]] = {
@@ -16,26 +27,44 @@ class PathsFinding:
         self.src: Hub
         self.goal: Hub
         self.queue: List[Hub] = []
-
-    def init_data(self) -> None:
-        for hub in self.input_data.hubs:
-            if hub.is_start:
-                self.src = hub
-            if hub.is_end:
-                self.goal = hub
+        self.init_algo()
+        self.get_hub_scores()
 
     def is_free_hub(self, hub: Hub, time: int) -> bool:
+        '''
+        Return True if the drone can go to the target hub in the right time
+        Params:
+            hub: Hub = The target hub to potentially reach the next turn
+            time: int = which turn the drone tries to go in this hub
+        Return value:
+            bool = True if it can go in the hub else it returns False
+        '''
         if (count := self.reservation_hub[hub.name].get(time)) is not None:
             return count < hub.max_drones
         return True
 
     def is_free_connection(self, connection: Connection, time: int) -> bool:
+        '''
+        Return True if the drone can pass by the target connection in the right time
+        Params:
+            connection: Connection = The target connection to potentially cross the next turn
+            time: int = which turn the drone tries to cross the connection
+        Return value:
+            bool = True if it can cross the connection else it returns False
+        '''
         if ((count := self.res_conn[connection.connection_id].get(time))
                 is not None):
             return count < connection.max_link_capacity
         return True
 
     def reserve_hub(self, hub: Hub, time: int, delta_t: int) -> None:
+        '''
+        Reserve the hub for the next turn to let know to other drones that the place is taken
+        Params:
+            hub: Hub = The target hub to reserve
+            time: int = current time
+            delta_t = difference between current time and the time we want to reserve
+        '''
         if not self.reservation_hub[hub.name].get(time + delta_t):
             self.reservation_hub[hub.name][time + delta_t] = 1
         else:
@@ -44,6 +73,12 @@ class PathsFinding:
     def reserve_connection(self,
                            connection: Connection,
                            time: int) -> None:
+        '''
+        Reserve the connection for the next turn to let know to other drones that the place is taken (mostly for Restricted)
+        Params:
+            connection: Connection = The target connection to reserve
+            time: int = current time
+        '''
         if not self.res_conn[connection.connection_id].get(time):
             self.res_conn[connection.connection_id][time] = 1
         else:
@@ -52,7 +87,15 @@ class PathsFinding:
     def get_available_neighbor(self,
                                curr_hub: Hub,
                                curr_time: int
-                               ) -> list[tuple[Hub, Connection, int]]:
+                               ) -> List[tuple[Hub, Connection, int]]:
+        '''
+        Get all available neighbor from a hub
+        Params:
+            curr_hub: Hub = the target hub to find their neighbor
+            curr_time: int = To look if the neighbor are free in this time
+        Return Value:
+            List[tuple[Hub, Connection, int]]: List of neighbor with the (next_hub, connection, delta_t)
+        '''
         neighbors_data = []
         for conn in self.input_data.connections:
             target_hub = None
@@ -75,6 +118,11 @@ class PathsFinding:
         return neighbors_data
 
     def get_path(self) -> List[Hub]:
+        '''
+        Get the path for the drone
+        Return value:
+            List[Hub]: The path
+        '''
         curr_place = self.src
         path: List[Hub] = [self.src]
         curr_time = 0
@@ -117,6 +165,9 @@ class PathsFinding:
         return path
 
     def init_algo(self) -> None:
+        '''
+        find the start hub and the goal hub from the list of hubs
+        '''
         for conn in self.input_data.connections:
             if conn.hub1.is_start:
                 self.src = conn.hub1
@@ -127,6 +178,10 @@ class PathsFinding:
                              "and an end to the graph")
 
     def get_hub_scores(self) -> None:
+        '''
+        Reverse Djikstra to know how many turn it needs to reach the goal
+        Create self.scores that store hub heuristics
+        '''
         scores = {hub.name: math.inf for hub in self.input_data.hubs}
         scores[self.goal.name] = 0
         queue = [self.goal]
@@ -157,10 +212,17 @@ class PathsFinding:
 
 
 def algo_path(input_data: Input_Data) -> Input_Data:
+    '''
+    Get path for each drones
+    Params:
+        input_data: Input_Data = a class that store a list of drones,
+            list of hubs and list of connections
+    Return value:
+        Input_Data = a class that store a list of drones with pathes,
+            list of hubs and list of connections
+    '''
     try:
         path = PathsFinding(input_data)
-        path.init_algo()
-        path.get_hub_scores()
     except Exception as e:
         raise ValueError(e)
 
@@ -169,5 +231,5 @@ def algo_path(input_data: Input_Data) -> Input_Data:
         if path:
             drone.path = drone_path
         else:
-            raise ValueError("An error occured in the algorythm")
+            raise ValueError("An error occured in the algorithm")
     return input_data
--- src/display/camera.py
+++ src/display/camera.py
@@ -7,6 +7,13 @@ class Camera:
                  screen_width: int,
                  screen_height: int
                  ) -> None:
+        '''
+        Initialiazed camera class to fit perfetly the graph in the window.
+        Params:
+            hubs: list[Hub] = List of hubs to get all possible positions
+            screen_width: int = the screen width of the window
+            screen_height: int = the screen height of the window
+        '''
         self.screen_width = screen_width
         self.screen_height = screen_height
         margin = 50
@@ -39,13 +46,32 @@ class Camera:
                           logical_x: float,
                           logical_y: float
                           ) -> tuple[int, int]:
+        '''
+        get the coords of anything with in the window like hub, connection drones etc.
+        Params:
+            logical_x: float = the x of the target to position
+            logical_y: float = the y of the target to position
+        Return values:
+            tuple[int, int] = the translated coords of target in the projection (x, y) 
+        '''
         screen_x = int(logical_x * self.scale + self.offset_x + self.add_x)
         screen_y = int(-logical_y * self.scale + self.offset_y + self.add_y)
         return screen_x, screen_y
 
     def update_screen_coords(self, add_x: int, add_y: int) -> None:
+        '''
+        for the mouse movement, to translate everything
+        Params:
+            add_x: int = the x value to add
+            add_y: int = the y value to add
+        '''
         self.add_x += add_x
         self.add_y += add_y
 
     def zoom(self, scroll: int) -> None:
+        '''
+        for the mouse scroll, to zoom
+        Params:
+            scroll: int = the scroll value to add
+        '''
         self.scale += scroll
